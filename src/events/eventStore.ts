d8366bde70ef8ea644b83c1179b78b21f9a6dfcd
nitin10 <nitiniszod10@gmail.com>
2026-06-29 01:01:50 +0530

    fix: DocuVerse auto-fix for src/events/eventStore.ts

--- src/events/eventStore.ts
+++ src/events/eventStore.ts
@@ -43,9 +43,9 @@ export function updateEventAssets(
     if (idx < 0) return;
 
     events[idx] = {
-       ...events[idx],
+      ...events[idx],
         assets: {
-           ...events[idx].assets,
+          ...events[idx].assets,
             [assetType]: assetId,
         },
     };
